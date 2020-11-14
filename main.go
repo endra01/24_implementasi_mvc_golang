cdb88df8865ae5e3859aa8097c1f39b738bc3a72
Endra Wahyudi B S <endrawahyudibs@gmail.com>
2020-11-14 22:22:06 +0700

    'Init'

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"MVC-Golang-3/app/controller"
+	"MVC-Golang-3/app/middleware"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+	router.Use(cors.Default())
+	router.POST("/api/v1/account/add", controller.CreateAccount)
+	router.POST("/api/v1/login", controller.Login)
+	router.GET("/api/v1/account", middleware.Auth, controller.GetAccount)
+	router.POST("/api/v1/transfer", middleware.Auth, controller.Transfer)
+	router.POST("/api/v1/withdraw", middleware.Auth, controller.Withdraw)
+	router.POST("/api/v1/deposit", middleware.Auth, controller.Deposit)
+	router.Run(":8081")
+}
